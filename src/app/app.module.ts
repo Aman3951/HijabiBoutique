574c555e1937a004b22737ae80afac3d51717d03
Aman <aman@siroi-solutions.com.my>
2018-03-07 22:48:42 +0800

    Added blank home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './modules/public/pages/home-page/home-page.component';
+import { HeaderComponent } from './modules/public/components/header/header.component';
+import { NavBarComponent } from './modules/public/components/nav-bar/nav-bar.component';
+import { HomeBodyComponent } from './modules/public/components/home-body/home-body.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HeaderComponent,
+    NavBarComponent,
+    HomeBodyComponent,
+
   ],
   imports: [
     BrowserModule
